6f95fbe8b32a465670f8fd43e941378b9149223f
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-09 21:16:41 +0100

    Primeres linies de codi

--- persones_desaparegudes_streamlit_app/main.py
+++ persones_desaparegudes_streamlit_app/main.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+
+desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
+provincies = pd.read_csv("data/provincies.csv")
+
+df_naixement = desapareguts.groupby(["Provincia.naixement"])["Provincia.naixement"].count().reset_index(name="count")
+df_naixement = df_naixement.merge(provincies, how="left", left_on="Provincia.naixement", right_on="nom_cat")
+df_naixement
