2031775716731a30d20fdd982f145034dd136788
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-10 19:26:19 +0100

    Update main.py

--- persones_desaparegudes_streamlit_app/main.py
+++ persones_desaparegudes_streamlit_app/main.py
@@ -15,5 +15,13 @@ provincies = pd.read_csv("data/provincies.csv")
 df_naixement = desapareguts.groupby(["Provincia.naixement"])["Provincia.naixement"].count().reset_index(name="count")
 df_naixement = df_naixement.merge(provincies, how="left", left_on="Provincia.naixement", right_on="nom_cat")
 
+# Create select box for the different maps
+choice = ['Mapa de les províncies de naixement de les persones desaparegudes',
+          'Mapa de les províncies habituals de les persones desaparegudes', 
+          'Mapa de les províncies on es van produir les desaparicions', 
+          'Mapa de les províncies on es van produir els afusellaments',
+          'Mapa de les províncies on es van localitzar els cosos de les persones desaparegudes']
+choice_selected = st.selectbox("Selecciona el mapa", choice, index=2)
+
 # Create map
 m = folium.Map(location=[40.41, -3.7], tiles='CartoDB positron', zoom_start=5)
