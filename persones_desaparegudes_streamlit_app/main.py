1335017869b37171614cd01585048f7ed7b29598
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-10 19:51:40 +0100

    Update main.py

--- persones_desaparegudes_streamlit_app/main.py
+++ persones_desaparegudes_streamlit_app/main.py
@@ -1,19 +1,20 @@
-import folium
+# import folium
 import pandas as pd
 import streamlit as st
-from streamlit_folium import folium_static
+# from streamlit_folium import folium_static
 
 
 # Config page
 st.set_page_config(layout="wide")
 
 # Load data
-desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
-provincies = pd.read_csv("data/provincies.csv")
+desapareguts = pd.read_csv("../data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
+provincies = pd.read_csv("../data/provincies.csv")
 
 # Join data with provinces information
 df_naixement = desapareguts.groupby(["Provincia.naixement"])["Provincia.naixement"].count().reset_index(name="count")
 df_naixement = df_naixement.merge(provincies, how="left", left_on="Provincia.naixement", right_on="nom_cat")
+st.dataframe(data=df_naixement, hide_index=True)
 
 # Create select box for the different maps
 choice = ['Mapa de les províncies de naixement de les persones desaparegudes',
@@ -24,4 +25,4 @@ choice = ['Mapa de les províncies de naixement de les persones desaparegudes',
 choice_selected = st.selectbox("Selecciona el mapa", choice, index=2)
 
 # Create map
-m = folium.Map(location=[40.41, -3.7], tiles='CartoDB positron', zoom_start=5)
+# m = folium.Map(location=[40.41, -3.7], tiles='CartoDB positron', zoom_start=5)
