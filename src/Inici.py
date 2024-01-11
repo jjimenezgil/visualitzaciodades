5887bcdd616c6b3b771b91839075b2e4fbbbe0d5
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-11 20:10:18 +0100

    Update Inici.py

--- src/Inici.py
+++ src/Inici.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="Desapareguts a la guerra civil Espanyola"
+    page_title="Inici"
 )
 
 st.markdown("# Desapareguts a la Guerra Civil Espanyola")
