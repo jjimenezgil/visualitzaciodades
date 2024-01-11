8d65973c0c88d29bdf064ca592cfc44fbba93adc
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-11 12:37:58 +0100

    Create Inici.py

---
+++ src/Inici.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Desapareguts a la guerra civil Espanyola"
+)
+
+st.markdown("# Desapareguts a la Guerra Civil Espanyola")
+
+desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
+
+st.dataframe(data=desapareguts)
