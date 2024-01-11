867af464ff29c4c18828a5707351a3fc28cde11a
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-11 18:44:15 +0100

    Update 2_🌍_Dades_Geogràfiques.py

--- src/pages/2_🌍_Dades_Geogràfiques.py
+++ src/pages/2_🌍_Dades_Geogràfiques.py
@@ -17,9 +17,13 @@ st.markdown("# Dades geogràfiques")
 # Load data
 desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
 provincies = pd.read_csv("data/provincies.csv")
+paisos = pd.read_csv("data/countries.csv")
 with open("data/spain-provinces.geojson") as f:
     geojson_data = json.load(f)
 geodf = geopandas.GeoDataFrame.from_features(geojson_data, crs="EPSG:4326")
+with open('data/countries.geojson') as f:
+    c = json.load(f)
+geodf_countries = geopandas.GeoDataFrame.from_features(c, crs="EPSG:4326")
 
 # Counts per province
 df_naixement = desapareguts.groupby(["Provincia.naixement"])["Provincia.naixement"].count().reset_index(name="countNaixement")
@@ -84,3 +88,13 @@ folium.GeoJson(geo_df_final, name="Provinces map", tooltip=tooltip).add_to(m)
 
 # Show map
 folium_static(m, width=1000, height=600)
+
+# Count and joins per country
+df_paisos = desapareguts.groupby(["Pais.desaparicio"])["Pais.desaparicio"].count().reset_index(name="count")
+df_paisos = df_paisos.merge(paisos, how="outer", left_on="Pais.desaparicio", right_on="nom_cat")
+geo_df_paisos = geodf_countries.merge(df_paisos, how="inner", left_on="name_es", right_on="name_es")
+
+# Country map
+m2 = folium.Map(location=[50.37, 15], tiles='CartoDB positron', zoom_start=2)
+folium.GeoJson(geo_df_paisos, name="Countries map").add_to(m2)
+folium_static(m2, width=1000, height=600)
