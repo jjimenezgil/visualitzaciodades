1eb2bb38ea2a3beea112c134a799b2a1bbdb3538
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-11 12:24:47 +0100

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="Visualització de dades",
+    page_title="Desapareguts a la guerra civil Espanyola",
     page_icon="📈",
 )
+
+st.markdown("# Desapareguts a la Guerra Civil Espanyola")
