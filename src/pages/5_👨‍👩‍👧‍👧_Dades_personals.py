e2513cdda6226a619efedf489c5a4e164a253c57
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-12 19:46:05 +0100

    Update 5_👨‍👩‍👧‍👧_Dades_personals.py

--- src/pages/5_👨‍👩‍👧‍👧_Dades_personals.py
+++ src/pages/5_👨‍👩‍👧‍👧_Dades_personals.py
@@ -1,1 +1,22 @@
+from wordcloud import WordCloud
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
 
+
+desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
+desapareguts_homes = desapareguts.loc[desapareguts["Sexe"]=="Home"]
+desapareguts_homes_professio = desapareguts_homes.loc[desapareguts["Professio"].notna()]
+
+professions = desapareguts_homes_professio["Professio"].tolist()
+professions = list(map(lambda x: x.split(" ")[0].strip(), professions))
+text = ' '.join(professions)
+
+# background_color="white"
+wordcloud = WordCloud(width = 700,
+                      height = 300,
+                      collocations=False,
+                      max_words=500).generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
