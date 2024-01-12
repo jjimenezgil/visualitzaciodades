e1bda562df66f747176b756a829b2dcd82428454
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-12 21:31:18 +0100

    Update 5_👨‍👩‍👧‍👧_Dades_personals.py

--- src/pages/5_👨‍👩‍👧‍👧_Dades_personals.py
+++ src/pages/5_👨‍👩‍👧‍👧_Dades_personals.py
@@ -10,18 +10,29 @@ st.set_page_config(page_title="Dades personals",
 
 # Text
 '''
-# Dades geogràfiques
+# Dades personals
 '''
 
+# Load data
 desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
 desapareguts_homes = desapareguts.loc[desapareguts["Sexe"]=="Home"]
 desapareguts_homes_professio = desapareguts_homes.loc[desapareguts["Professio"].notna()]
+
+# Histogram per sex
+counts = desapareguts.groupby(["Sexe"])["Sexe"].count()
+y = [counts["Home"], counts["Dona"]]
+x = ["Home", "Dona"]
+fig1, ax1 = plt.subplots()
+ax1.bar(x, y, align='center')
+st.pyplot(fig1)
+
+
 
 professions = desapareguts_homes_professio["Professio"].tolist()
 professions = list(map(lambda x: x.split(" ")[0].strip(), professions))
 text = ' '.join(professions)
 
-# background_color="white"
+
 wordcloud = WordCloud(width = 700,
                       height = 300,
                       collocations=False,
@@ -30,5 +41,5 @@ wordcloud = WordCloud(width = 700,
 # Display the generated image:
 fig, ax = plt.subplots()
 ax.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
+ax.axis("off")
 st.pyplot(fig)
