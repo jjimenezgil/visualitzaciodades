efb22c869a2a95cb71dc2166855a2d66d08e0cb7
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-13 19:31:17 +0100

    Update Inici.py

--- src/Inici.py
+++ src/Inici.py
@@ -6,8 +6,14 @@ st.set_page_config(
     page_title="Inici"
 )
 
-st.markdown("# Desapareguts a la Guerra Civil Espanyola")
+'''
+# Desapareguts a la Guerra Civil Espanyola
+
+En aquesta app d'Streamlit, corresponent a la PRA final de l'assignatura de Visualització de Dades 
+del Màster en Ciència de Dades de la UOC, explorarem el Cens de persones desaparegudes durant la 
+Guerra Civil Espanyola, disponible al [Portal de dades obertes de la Generalitat de Catalunya](https://analisi.transparenciacatalunya.cat/Legislaci-just-cia/Cens-de-persones-desaparegudes-durant-la-Guerra-Ci/u2ix-2jr6/about_data)
+eh
+'''
 
 desapareguts = pd.read_csv("data/Cens_de_persones_desaparegudes_durant_la_Guerra_Civil_clean.csv")
-
 st.dataframe(data=desapareguts)
