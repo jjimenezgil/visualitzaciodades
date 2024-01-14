9b4369f599df2fe48413a65fde0b9b8a404986d3
jjimenezgil <152742330+jjimenezgil@users.noreply.github.com>
2024-01-14 18:26:41 +0100

    Update 3_⏱️_Dades_cronològiques.py

--- src/pages/3_⏱️_Dades_cronològiques.py
+++ src/pages/3_⏱️_Dades_cronològiques.py
@@ -45,3 +45,10 @@ fig.add_shape(
     y1=2500,
 )
 st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("""<p class="big-font">
+Trobem que el pitjor any, tant per al gènere masculí com femení, és el 1938, amb 2533 i 38 desaparicions respectivament. A partir d'aquí el nombre
+de desaparicions va disminuint, tot i que se'n continuen produint durant la Dictadura però en quantitats molt menors. Per a les dones la darrera 
+desaparició es va produir a l'any 1948, però per als homes en trobem 1 o 2 a l'any fins a arribar al 1973, és a dir, fins gairebé el final de la 
+Dictadura, que es va produir en 1975.
+</p>""", unsafe_allow_html=True)
